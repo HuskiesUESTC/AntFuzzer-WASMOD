21c864faca46a0d08c43e386258aa3520a163bc5
JiangTianxing <972786892@qq.com>
2021-09-07 12:30:06 +0800

    管理AFL进程

--- src/main/java/edu/uestc/antfuzzer/framework/util/middleware/eos/after/EndAFL.java
+++ src/main/java/edu/uestc/antfuzzer/framework/util/middleware/eos/after/EndAFL.java
@@ -3,6 +3,7 @@ package edu.uestc.antfuzzer.framework.util.middleware.eos.after;
 import edu.uestc.antfuzzer.framework.annotation.Autowired;
 import edu.uestc.antfuzzer.framework.annotation.Component;
 import edu.uestc.antfuzzer.framework.util.AFLUtil;
+import edu.uestc.antfuzzer.framework.util.EnvironmentUtil;
 import edu.uestc.antfuzzer.framework.util.middleware.AfterCheck;
 
 import java.lang.reflect.InvocationTargetException;
@@ -13,9 +14,14 @@ public class EndAFL extends AfterCheck {
     @Autowired
     private AFLUtil aflUtil;
 
+    @Autowired
+    private EnvironmentUtil environmentUtil;
+
     @Override
     protected boolean currentCheck() throws InvocationTargetException, IllegalAccessException {
-        aflUtil.endAFL();
+        if (environmentUtil.isCurrentActionUsingAFLDriver()) {
+            aflUtil.endAFL();
+        }
         return true;
     }
 }
--- src/main/java/edu/uestc/antfuzzer/framework/util/middleware/eos/before/InitEOSFuzzingResult.java
+++ src/main/java/edu/uestc/antfuzzer/framework/util/middleware/eos/before/InitEOSFuzzingResult.java
@@ -4,8 +4,10 @@ import edu.uestc.antfuzzer.framework.annotation.Autowired;
 import edu.uestc.antfuzzer.framework.annotation.Component;
 import edu.uestc.antfuzzer.framework.bean.result.EOSFuzzingResult;
 import edu.uestc.antfuzzer.framework.util.EnvironmentUtil;
+import edu.uestc.antfuzzer.framework.util.PipeUtil;
 import edu.uestc.antfuzzer.framework.util.middleware.BeforeCheck;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -14,14 +16,17 @@ public class InitEOSFuzzingResult extends BeforeCheck {
 
     @Autowired
     private EnvironmentUtil environmentUtil;
+    @Autowired
+    private PipeUtil pipeUtil;
 
     @Override
-    protected boolean currentCheck() {
+    protected boolean currentCheck() throws IOException {
         EOSFuzzingResult eosFuzzingResult = new EOSFuzzingResult();
         eosFuzzingResult.setSmartContracts(new LinkedList<>());
         eosFuzzingResult.setStartTime(System.currentTimeMillis());
         eosFuzzingResult.setVulnerableSmartContracts(new HashMap<>());
         environmentUtil.setEosFuzzingResult(eosFuzzingResult);
+        pipeUtil.execute("kill -9 `ps -ef |grep redis|awk '{print $2}'`");
         return true;
     }
 }
